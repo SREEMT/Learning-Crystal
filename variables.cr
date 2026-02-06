b24922eb178f1b471fe810ea61f1115a179e53a7
SREEMT <tmeers101@gmail.com>
2026-02-06 10:27:14 -0500

    Added variables.cr

---
+++ variables.cr
@@ -0,0 +1,21 @@
+message = "Hello there"
+
+puts message
+puts message
+puts message
+
+p! typeof(message)
+
+message = "Hello Penny!"
+
+p! message
+
+message = "Hello Sheldon!"
+
+p! message
+
+p! message, typeof(message)
+
+message = 73
+
+p! message, typeof(message)
